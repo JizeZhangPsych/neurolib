d55f8922ad328625aaa141832aa71bfcf137e0f2
Jize Zhang <jizhang@hbaws45.localdomain>
2025-05-03 14:26:17 +0100

    v0.2 cleanup

--- fastr.m
+++
@@ -1,9 +0,0 @@
-function processed_data = fastr(eeg_path, varargin)
-    eegdata, header = readedf(eeg_path);
-    EEG = struct();
-    EEG.data = eegdata;
-    EEG.srate = header.samplerate(1);
-    EEG.pnts = size(eegdata, 2);
-    EEG.events = [];
-    processed_data = fmrib_fastr(EEG, varargin{:});
-end
